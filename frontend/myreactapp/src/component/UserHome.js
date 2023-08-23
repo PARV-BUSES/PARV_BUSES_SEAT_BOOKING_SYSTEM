3df23dd18586e8542a091bb4d6169c484999df5c
pandharevitthal1 <pandharevitthal1@gmail.com>
2023-08-23 16:25:23 +0530

    get buses on searching functionality

--- frontend/myreactapp/src/component/UserHome.js
+++ frontend/myreactapp/src/component/UserHome.js
@@ -5,16 +5,52 @@ import axios from "axios";
 function UserHome() {
   const [stationList, setStationList] = useState([]);
 
+  // const[getBusData,setGetBusData] = useState({
+  //   from:0,
+  //   to:0,
+  //   date:""
+  // })
+
+  const [from, setFrom] = useState(0);
+  const [to, setTo] = useState(0);
+
+  const [buses,setBuses] = useState([])
+
+  const handleGetBusDataChange = (e) => {
+    if (e.target.name == "from") {
+      setFrom(e.target.value);
+    } else if (e.target.name == "to") {
+      setTo(e.target.value);
+    }
+
+    // console.log(from)
+    // console.log(to)
+  };
+
   useEffect(() => {
     // debugger
-    console.log("in use");
+    // console.log("in use");
     axios
       .get("http://localhost:8080/station/getstations")
       .then((response) => setStationList(response.data))
       .catch((error) => console.log(error + "some error"));
   }, []);
 
-  console.log(stationList);
+  const getBuses = () => {
+    const getBusData = {
+      from: from,
+      to: to,
+    };
+
+    console.log(getBusData)
+
+    axios
+      .post("http://localhost:8080/bus/getbus", getBusData)
+      .then((response) => setBuses(response.data))
+      .catch((error) => console.log(error + "some error"));
+  };
+
+  // console.log(stationList);
 
   return (
     <>
@@ -30,19 +66,15 @@ function UserHome() {
             {" "}
             <b>From: </b>{" "}
           </label>
-          <select className="form-control" style={{ width: "260px" }}>
-            {/* <option value="karad" key="">
-            karad
-          </option>
-          <option value="pune" key="">
-            pune
-          </option>
-          <option value="mumbai" key="">
-            mumbai
-          </option> */}
+          <select
+            className="form-control"
+            style={{ width: "260px" }}
+            name="from"
+            onChange={handleGetBusDataChange}>
+            <option>select</option>
             {stationList.map((e) => {
               return (
-                <option value={e.station_name} key={e.id}>
+                <option value={e.id} key={e.id}>
                   {e.station_name}
                 </option>
               );
@@ -56,31 +88,35 @@ function UserHome() {
             {" "}
             <b>To: </b>{" "}
           </label>
-          <select className="form-control" style={{ width: "260px" }}>
-            {/* <option value="panvel" key="">
-              panvel
-            </option>
-            <option value="latur" key="">
-              latur
-            </option>
-            <option value="thane" key="">
-              thane
-            </option> */}
+          <select
+            name="to"
+            onChange={handleGetBusDataChange}
+            className="form-control"
+            style={{ width: "260px" }}>
+            <option>select</option>
 
             {stationList.map((e) => {
               return (
-                <option value={e.station_name} key={e.id}>
+                <option value={e.id} key={e.id}>
                   {e.station_name}
                 </option>
               );
             })}
           </select>
         </div>
-        <button type="submit" class="btn btn-primary mb-2">
+        <button onClick={getBuses} type="button" class="btn btn-primary mb-2">
           Search
         </button>
       </form>
-      <BusComp from="latur" />
+      {
+        // console.log(buses.length)
+        buses.map((e)=>{
+          console.log(e)
+          return(
+            <BusComp data={e} />
+          )
+        })
+      }
     </>
   );
 }
